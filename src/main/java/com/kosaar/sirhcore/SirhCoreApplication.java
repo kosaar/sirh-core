56be9d9e73f95b2c18bdf6cba65def7d270a6697
Ibrahim Mohamed <mohamed.ibrahim@horoquartz.fr>
2023-06-08 11:06:55 +0200

    first commit

---
+++ src/main/java/com/kosaar/sirhcore/SirhCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kosaar.sirhcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirhCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirhCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosaar/sirhcore/SirhCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosaar.sirhcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirhCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
